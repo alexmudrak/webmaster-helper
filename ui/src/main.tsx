37120ea82fc12f474c080cfcfc2a6ff490bb78e8
alexmudrak <95851199+alexmudrak@users.noreply.github.com>
2024-02-07 20:30:09 +0200

    chore: update django version

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
